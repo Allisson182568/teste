7c38b2faa28f0f731bd50f38d33dfcd3f4021957
allisson <allisson182568@gmail.com>
2022-04-09 16:19:45 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/grupodantas/teste/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.grupodantas.teste
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  //inicializacao admob
+  MobileAds.instance.initialize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  BannerAd? bannerAd;
+
+  bool isLoaded = false;
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    bannerAd = BannerAd(
+      size: AdSize.banner,
+      adUnitId: "ca-app-pub-3795771068897786/3362962727",
+      listener: BannerAdListener(onAdLoaded: (ad) {
+        setState(() {
+          isLoaded = true;
+        });
+        print("Banner Ad Loaded");
+      }, onAdFailedToLoad: (ad, error) {
+        ad.dispose();
+      }),
+      request: AdRequest(),
+    );
+    bannerAd!.load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+
+      bottomNavigationBar: Container(
+          height: 60,
+          child: Column(
+            children: [
+              Spacer(),
+              isLoaded
+                  ? Container(
+                height: 60,
+                width: 468,
+                child: AdWidget(
+                  ad: bannerAd!,
+                ),
+              )
+                  : SizedBox()
+            ],
+          )
+      ),
+    );
+  }
+}
